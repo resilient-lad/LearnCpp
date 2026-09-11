2ba962bbdf33de4c97c70500c7fa478d86306ff0
Jeeva <jeevaganjegatheesan02@gmail.com>
2026-09-11 22:18:45 +0530

    learning 11-09-2026

--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+// #include <random>
 
 int main(){
-    int a{}, b{5};
-    // std::cout<< "Enter numbers to be stored: ";
-    // std::cin >> a >>b;
-    std::cout << "You entered " <<a<<" and " <<b;
-    return 0;
+    // int guessing_number {};
+    // std::cin >> guessing_number;
+    std::cout << "Hello world";
 }

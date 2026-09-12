d45159e88f52302b56b7fc68583de3c58c51b948
Jeeva <jeevaganjegatheesan02@gmail.com>
2026-09-12 23:04:18 +0530

    Learned on 20260912

--- test.cpp
+++ test.cpp
@@ -3,8 +3,24 @@
 #include <string>
 // #include <random>
 
+int debugger(){
+    int test{5};
+    std::cout<<"Test parameter" << test << "\n";
+    return 1;
+}
+
+void printvalue(int x){
+    std::cout<<"Entered value is: "<<x<<"\n";
+}
+
 int main(){
     // int guessing_number {};
     // std::cin >> guessing_number;
     std::cout << "Hello world";
+    debugger();
+    std::cout<< "after execution of continue";
+    printvalue(5);
+    printvalue(6);
+    printvalue(7);
+    return 0;
 }
